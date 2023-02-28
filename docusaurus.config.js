a3b62837e9710efc3e4303494bb9eab2d61b748b
Zikani Nyirenda Mwase <zikani.nmwase@ymail.com>
2023-03-01 00:10:40 +0200

    fix: fix baseURL

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,7 +14,7 @@ const config = {
   url: 'https://learn.geekquest.mw',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/learn.geekquest.mw/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
